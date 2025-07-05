824075439d46540cc2ef13c75dae115cb75aee00
Abdelrahman Reyad <92989840+Abdelrahman-7z7@users.noreply.github.com>
2025-07-05 18:34:05 +0300

    adjusting the supabase configuration - since there is no sign-up or login in this application

--- src/config/supabaseConfig.ts
+++ src/config/supabaseConfig.ts
@@ -1,5 +1,5 @@
 import {createClient, SupabaseClient} from '@supabase/supabase-js';
-import AppError from '../utils/AppError';
+import { Database } from '../types/supabase';
 import * as dotenv from 'dotenv'; 
 
 dotenv.config({path: './.env'})
@@ -12,17 +12,18 @@ if(!SUPABASE_URL || !SERVICE_ROLE_KEY || !SERVICE_ROLE_KEY_SECRET){
     throw new Error('Missing Supabase environment variables')
 }
 
+//since we dont have a login and sign-up then no need for creating with client
 // (token:string) type of the paramter : SupabaseClient is the return type of the function
-export const createSupabaseWithUser = (token : string):SupabaseClient => {
-    return createClient(SUPABASE_URL, SERVICE_ROLE_KEY,  {
-        global: {
-            headers: {
-        Authorization: `Bearer ${token}`,
-            }
-        }
-    })
-} 
+// export const createSupabaseWithUser = (token : string):SupabaseClient => {
+//     return createClient(SUPABASE_URL, SERVICE_ROLE_KEY,  {
+//         global: {
+//             headers: {
+//         Authorization: `Bearer ${token}`,
+//             }
+//         }
+//     })
+// } 
 
-export const supabase: SupabaseClient = createClient(SUPABASE_URL, SERVICE_ROLE_KEY);
-export const supabaseSecret: SupabaseClient = createClient(SUPABASE_URL, SERVICE_ROLE_KEY_SECRET)
+export const supabase: SupabaseClient = createClient<Database>(SUPABASE_URL, SERVICE_ROLE_KEY);
+export const supabaseSecret: SupabaseClient = createClient<Database>(SUPABASE_URL, SERVICE_ROLE_KEY_SECRET)
 
